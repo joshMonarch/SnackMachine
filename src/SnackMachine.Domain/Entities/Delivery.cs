63e6b9ad7f371f369034d89bcfeedcdaf5004bca
joshMonarch <sesitado@gmail.com>
2026-01-31 19:32:45 +0100

    :card_file_box: Created Entities

---
+++ src/SnackMachine.Domain/Entities/Delivery.cs
@@ -0,0 +1,14 @@
+﻿using SnackMachine.Domain.Enums;
+
+namespace SnackMachine.Domain.Entities
+{
+    public class Delivery
+    {
+        public int Id { get; private set; }
+        public int ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public Status status { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+    }
+}
---
+++ src/SnackMachine.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SnackMachine.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string? Username { get; private set; }
+        public string? Email { get; private set; }
+        public string? PasswordHash { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+    }
+}
