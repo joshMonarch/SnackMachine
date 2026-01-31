86ef10887458da305834b9a28eeea65f95cbf30d
joshMonarch <sesitado@gmail.com>
2026-01-31 22:09:41 +0100

    :hammer: Created mappers

---
+++ src/SnackMachine.Application/Mappers/DeliveryMapper/DeliveryToEntity.cs
@@ -0,0 +1,17 @@
+﻿using SnackMachine.Application.DTOs.DeliveryDTO;
+using SnackMachine.Domain.Entities;
+
+namespace SnackMachine.Application.Mappers.DeliveryMapper
+{
+    public static class DeliveryToEntity
+    {
+        public static Delivery ToDto (CreateDeliveryDto createDeliveryDto)
+        {
+            return new Delivery(
+                createDeliveryDto.ProductId,
+                createDeliveryDto.Quantity,
+                createDeliveryDto.Status
+            );
+        }
+    }
+}
---
+++ src/SnackMachine.Application/Mappers/ProductMapper/ProductToEntity.cs
@@ -0,0 +1,17 @@
+﻿using SnackMachine.Application.DTOs.ProductDTOs;
+using SnackMachine.Domain.Entities;
+
+namespace SnackMachine.Application.Mappers.ProductMapper
+{
+    public static class ProductToEntity
+    {
+        public static Product ToEntity(CreateProductDto createProductDto)
+        {
+            return new Product(
+                createProductDto.Name,
+                createProductDto.Price,
+                createProductDto.Stock
+            );
+        }
+    }
+}
